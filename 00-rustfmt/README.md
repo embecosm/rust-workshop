1c83484e6a8918819e3c1e856e1848d028c45b48
flip1995 <philipp.krones@embecosm.com>
2020-12-15 19:16:25 +0100

    Add rustfmt task

---
+++ 00-rustfmt/README.md
@@ -0,0 +1,9 @@
+# Rustfmt
+
+This is mostly a check, that `cargo rustfmt` works.
+
+## Task
+
+1. Look at the `src/main.rs` file
+2. Run `cargo fmt` inside `00-rustfmt`
+3. Verify that `src/main.rs` is now formatted
---
+++ 00-rustfmt/src/main.rs
@@ -0,0 +1,26 @@
+fn main()
+{
+    let mess = "Hello, World!";
+    println!  (
+
+"this is"
+                );
+    let
+        _an
+= 42u32
+  ;
+
+    absolute   (
+        mess );
+}
+
+fn absolute(
+hello:
+  &str)
+
+{
+    println!(
+    "{}",
+           hello)
+        ;
+}
